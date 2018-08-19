fbc4bb7b9ea8ce2b008e55af015beae5cbfedf60
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-08-18 21:17:04 -0500

    Feat - Update. Adding string tutorials

--- pythonTutorial.py
+++ pythonTutorial.py
@@ -1,2 +1,12 @@
-name = "Am learning"
+## Python tutorials
+
+name = "am learning"
 print(name.title())
+
+# Calling python method title on the variable to make it title
+# how to remove extra space from the variable
+
+favorite_language = ' python '
+print(favorite_language)
+favorite_language = favorite_language.rstrip();
+print(favorite_language)
