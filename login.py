10a99752237d9c18981bf9f301f05ea15e209648
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-08-21 18:45:04 -0500

    Feat - New Feature. Adding flask tutorial kind of hello world program

--- login.py
+++ login.py
@@ -6,4 +6,6 @@ def home():
 	return "My code is working! "
 
 if __name__ == "__main__":
+	## host meaning - Address of the computer and its port. Website or
+	## the location is the client
 	app.run(host='0.0.0.0', port=8000)
